8983176902fd34b892a9beb08880c5c178195d43
Alexandros Koukis <alexandroskoukis@gmail.com>
2019-06-16 14:33:32 +0300

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, View, Button} from 'react-native';
+import styles from '../styles';
+
+export default class Home extends React.Component {
+    state = {
+        count: 10
+    };
+
+    add = () => {
+        console.log("adding");
+        let num = this.state.count + 1;
+        this.setState({count: num});
+    };
+    minus = () => {
+        console.log("substracting");
+        let num = this.state.count - 1;
+        this.setState({count: num});
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>How many apps are we going to build? {this.state.count}</Text>
+                <Button title="Add" onPress={() => this.add()}/>
+                <Button title="Remove" onPress={() => this.minus()}/>
+            </View>
+        );
+    }
+}
+
